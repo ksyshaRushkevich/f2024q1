d230963f77cb5b9d5019457bff5e484b39b45617
ksysha <ksysha>
2024-10-11 11:28:35 +0300

    add:machineDescription

---
+++ object.ts
@@ -0,0 +1,12 @@
+const machineDescription = {
+    title: "BMW M8",
+    securitySystems: "ABS",
+    cushions: "передние",
+    aidSystems: "датчик дождя,камера заднего вида",
+    salon: "light"
+}
+machineDescription.salon = "темный"
+
+console.log(machineDescription.cushions)
+
+
