ece4d33a1b96a44ec616ac0eb41ae74f3d00e9fd
ksysha <ksysha>
2024-11-15 11:08:00 +0300

    add: white party

---
+++ o.js
@@ -0,0 +1,14 @@
+const dgfds=[1,2,3,4]
+const emtyarray = []
+for(i of dgfds){
+    emtyarray.push(i+52)
+}
+//console.log(emtyarray)
+
+function beyonce(PDidi){
+    const JK =[]
+    for(i of PDidi)
+        JK.push(i+51)
+    return JK
+}
+console.log(beyonce(dgfds))
