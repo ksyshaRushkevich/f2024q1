bbe77e5ecd2cc34d7e61c648a7ae6f5227e6ca99
ksysha <ksysha>
2024-11-22 11:45:21 +0300

    add:sort array

---
+++ sort.js
@@ -0,0 +1,8 @@
+const words=['chocopaille','insulin','moues','masha','bear','bunny']
+ const resuilUp = words.sort((a,b)=> a-b)
+ //console.log(resuilUp)
+
+ const resuilDown = words.sort((a,b)=>a-b)
+ const resersed = resuilDown.reserve()
+ console.log(resuilDown)
+
